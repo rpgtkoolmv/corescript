4ec82f14ce1c09bd290db5f80ef7fabc0ad9f15a
Nio Kasgami <devil_nicolas@hotmail.com>
2017-04-13 22:54:42 -0400

    Added docs
    Added documentations to the Scene_Base class.

--- js/rpg_scenes/Scene_Base.js
+++ js/rpg_scenes/Scene_Base.js
@@ -1,8 +1,14 @@
+/=============================================================================
+// ■ Scene_Base V.1.4.0 (community-1.1b)
 //-----------------------------------------------------------------------------
-// Scene_Base
-//
-// The superclass of all scenes within the game.
 
+/**
+ * The Superclass of all scene within the game.
+ * 
+ * @class Scene_Base
+ * @constructor 
+ * @extends Stage
+ */
 function Scene_Base() {
     this.initialize.apply(this, arguments);
 }
@@ -10,6 +16,13 @@ function Scene_Base() {
 Scene_Base.prototype = Object.create(Stage.prototype);
 Scene_Base.prototype.constructor = Scene_Base;
 
+
+/**
+ * Create a instance of Scene_Base.
+ * 
+ * @constructor
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.initialize = function() {
     Stage.prototype.initialize.call(this);
     this._active = false;
@@ -19,45 +32,115 @@ Scene_Base.prototype.initialize = function() {
     this._imageReservationId = Utils.generateRuntimeId();
 };
 
+/**
+ * Attach a reservation to the reserve queue.
+ * 
+ * @method attachReservation
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.attachReservation = function() {
     ImageManager.setDefaultReservationId(this._imageReservationId);
 };
 
+/**
+ * Remove the reservation from the Reserve queue.
+ * 
+ * @method detachReservation
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.detachReservation = function() {
     ImageManager.releaseReservation(this._imageReservationId);
 };
 
+/**
+ * Create the components and add them to the rendering process.
+ * 
+ * @method create
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.create = function() {
 };
 
+/**
+ * Returns wether the scene is active or not.
+ * 
+ * @method isActive
+ * @memberof Scene_Base
+ * @return {Boolean} return true if the scene is active
+ */
 Scene_Base.prototype.isActive = function() {
     return this._active;
 };
 
+/**
+ * Return wether the scene is ready to start or not.
+ * 
+ * @method isReady
+ * @memberof Scene_Base
+ * @return {Boolean} Return true if the scene is ready to start
+ */
 Scene_Base.prototype.isReady = function() {
     return ImageManager.isReady();
 };
 
+/**
+ * Start the scene processing.
+ * 
+ * @method start
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.start = function() {
     this._active = true;
 };
 
+/**
+ * Update the scene processing each new frame.
+ * 
+ * @method update
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.update = function() {
     this.updateFade();
     this.updateChildren();
+    AudioManager.checkErrors();
 };
 
+/**
+ * Stop the scene processing.
+ * 
+ * @method stop
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.stop = function() {
     this._active = false;
 };
 
+/**
+ * Return wether the scene is busy or not.
+ * 
+ * @method isBusy
+ * @return {boolean} true if the scene is currently busy doing a fade
+ */
 Scene_Base.prototype.isBusy = function() {
     return this._fadeDuration > 0;
 };
 
+/**
+ * Terminate the scene before switching to a another scene.
+ * 
+ * @method terminate
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.terminate = function() {
 };
 
+/**
+ * Create the layer for the windows children
+ * and add it to the rendering process.
+ * 
+ * @method createWindowLayer
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.createWindowLayer = function() {
     var width = Graphics.boxWidth;
     var height = Graphics.boxHeight;
@@ -68,10 +151,25 @@ Scene_Base.prototype.createWindowLayer = function() {
     this.addChild(this._windowLayer);
 };
 
+/**
+ * Add the children window to the windowLayer processing.
+ * 
+ * @method addWindow
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.addWindow = function(window) {
     this._windowLayer.addChild(window);
 };
 
+/**
+ * Request a fadeIn screen process.
+ * 
+ * @method startFadeIn
+ * @param {Number} [duration=30] The time the process will take for fadeIn the screen
+ * @param {Boolean} [white=false] If true the fadein will be process with a white color else it's will be black
+ * 
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.startFadeIn = function(duration, white) {
     this.createFadeSprite(white);
     this._fadeSign = 1;
@@ -79,6 +177,15 @@ Scene_Base.prototype.startFadeIn = function(duration, white) {
     this._fadeSprite.opacity = 255;
 };
 
+/**
+ * Request a fadeOut screen process.
+ * 
+ * @method startFadeOut
+ * @param {Number} [duration=30] The time the process will take for fadeOut the screen
+ * @param {boolean} [white=false] If true the fadeOut will be process with a white color else it's will be black
+ * 
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.startFadeOut = function(duration, white) {
     this.createFadeSprite(white);
     this._fadeSign = -1;
@@ -86,6 +193,13 @@ Scene_Base.prototype.startFadeOut = function(duration, white) {
     this._fadeSprite.opacity = 0;
 };
 
+/**
+ * Create a Screen sprite for the fadein and fadeOut purpose and
+ * add it to the rendering process.
+ * 
+ * @method createFadeSprite
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.createFadeSprite = function(white) {
     if (!this._fadeSprite) {
         this._fadeSprite = new ScreenSprite();
@@ -98,6 +212,12 @@ Scene_Base.prototype.createFadeSprite = function(white) {
     }
 };
 
+/**
+ * Update the screen fade processing.
+ * 
+ * @method updateFade
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.updateFade = function() {
     if (this._fadeDuration > 0) {
         var d = this._fadeDuration;
@@ -110,6 +230,12 @@ Scene_Base.prototype.updateFade = function() {
     }
 };
 
+/**
+ * Update the children of the scene EACH frame.
+ * 
+ * @method updateChildren
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.updateChildren = function() {
     this.children.forEach(function(child) {
         if (child.update) {
@@ -118,16 +244,35 @@ Scene_Base.prototype.updateChildren = function() {
     });
 };
 
+/**
+ * Pop the scene from the stack array and switch to the
+ * previous scene.
+ * 
+ * @method popScene
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.popScene = function() {
     SceneManager.pop();
 };
 
+/**
+ * Check wether the game should be triggering a gameover.
+ * 
+ * @method checkGameover
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.checkGameover = function() {
     if ($gameParty.isAllDead()) {
         SceneManager.goto(Scene_Gameover);
     }
 };
 
+/**
+ * Slowly fade out all the visual and audio of the scene.
+ * 
+ * @method fadeOutAll
+ * @memberof Scene_Base
+ */
 Scene_Base.prototype.fadeOutAll = function() {
     var time = this.slowFadeSpeed() / 60;
     AudioManager.fadeOutBgm(time);
@@ -136,10 +281,24 @@ Scene_Base.prototype.fadeOutAll = function() {
     this.startFadeOut(this.slowFadeSpeed());
 };
 
+/**
+ * Return the screen fade speed value.
+ * 
+ * @method fadeSpeed
+ * @memberof Scene_Base
+ * @return {Number} Return the fade speed
+ */
 Scene_Base.prototype.fadeSpeed = function() {
     return 24;
 };
 
+/**
+ * Return a slow screen fade speed value.
+ * 
+ * @method slowFadeSpeed
+ * @memberof Scene_Base
+ * @return {Number} Return the fade speed.
+ */
 Scene_Base.prototype.slowFadeSpeed = function() {
     return this.fadeSpeed() * 2;
 };
