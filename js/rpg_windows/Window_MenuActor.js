b638b3b1bce6afde3fc0a8e2c1632c98a54bb574
Sigureya <sigureya1208@gmail.com>
2018-05-20 23:24:17 +0900

    setCursorAllに関連した修正
    Although we were able to take measures against double updating of the cursor, a little uneasy remains.
    Also, somewhat doubt whether to allow Window to keep Game_Action.
    Required code review.
    
    カーソルの二重更新対策はできたが、やや不安は残る。
    また、WindowにGame_Actionを保持させていいか、やや疑問。
    要コードレビュー。

--- js/rpg_windows/Window_MenuActor.js
+++ js/rpg_windows/Window_MenuActor.js
@@ -13,6 +13,11 @@ Window_MenuActor.prototype.constructor = Window_MenuActor;
 Window_MenuActor.prototype.initialize = function() {
     Window_MenuStatus.prototype.initialize.call(this, 0, 0);
     this.hide();
+    this._action =null;
+};
+
+Window_MenuActor.prototype.action =function(){
+    return this._action;
 };
 
 Window_MenuActor.prototype.processOk = function() {
@@ -26,23 +31,35 @@ Window_MenuActor.prototype.selectLast = function() {
     this.select($gameParty.targetActor().index() || 0);
 };
 
-Window_MenuActor.prototype.selectForItem = function(item) {
+Window_MenuActor.prototype.selectDefaultTarget =function(){
+    if(!this._cursorAll){
+        if(this._action.isForUser()){
+            this.select(this._action.subject().index());
+        }else{
+            this.selectLast();
+        }    
+    }
+};
+
+Window_MenuActor.prototype.needsSelection =function(){
+    var action = this._action;
+    if(this._action.isForUser() && this._action.isItem() ){
+        return true;
+    }
+    return this._action.needsSelection();
+};
+
+Window_MenuActor.prototype.isActionForAll=function(){
+    return !this._action.isForOne();
+};
+
+Window_MenuActor.prototype.selectForItem =function(item){
     var actor = $gameParty.menuActor();
     var action = new Game_Action(actor);
     action.setItemObject(item);
-    this.setCursorFixed(false);
-    this.setCursorAll(false);
-    if (action.isForUser()) {
-        if (DataManager.isSkill(item)) {
-            this.setCursorFixed(true);
-            this.select(actor.index());
-        } else {
-            this.selectLast();
-        }
-    } else if (action.isForAll()) {
-        this.setCursorAll(true);
-        this.select(0);
-    } else {
-        this.selectLast();
-    }
+    this._action = action;
+
+    this.setCursorFixed(!this.needsSelection());
+    this.setCursorAll(this.isActionForAll());
+    this.selectDefaultTarget();
 };
