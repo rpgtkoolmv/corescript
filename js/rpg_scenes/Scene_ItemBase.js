ccb95562d4376936acb34b7cfc7ccb6edb3f2a97
Sigureya <sigureya1208@gmail.com>
2018-05-21 15:13:51 +0900

    Scene_ItemBase add action()fix

--- js/rpg_scenes/Scene_ItemBase.js
+++ js/rpg_scenes/Scene_ItemBase.js
@@ -61,13 +61,13 @@ Scene_ItemBase.prototype.onActorCancel = function() {
     this.hideSubWindow(this._actorWindow);
 };
 
-Scene_ItemBase.prototype.determineItem = function() {
-    var action = new Game_Action(this.user());
-    var item = this.item();
-    action.setItemObject(item);
+Scene_Skill.prototype.determineItem = function() {
+    var action = this.action();
+
     if (action.isForFriend()) {
         this.showSubWindow(this._actorWindow);
         this._actorWindow.selectForItem(this.item());
+
     } else {
         this.useItem();
         this.activateItemWindow();
@@ -88,9 +88,14 @@ Scene_ItemBase.prototype.activateItemWindow = function() {
     this._itemWindow.activate();
 };
 
-Scene_ItemBase.prototype.itemTargetActors = function() {
+Scene_ItemBase.prototype.action=function(){
     var action = new Game_Action(this.user());
     action.setItemObject(this.item());
+    return action;
+};
+
+Scene_ItemBase.prototype.itemTargetActors =function(){
+    var action = this.action();
     if (!action.isForFriend()) {
         return [];
     } else if (action.isForAll()) {
@@ -105,21 +110,21 @@ Scene_ItemBase.prototype.canUse = function() {
 };
 
 Scene_ItemBase.prototype.isItemEffectsValid = function() {
-    var action = new Game_Action(this.user());
-    action.setItemObject(this.item());
+    var action = this.action();
     return this.itemTargetActors().some(function(target) {
         return action.testApply(target);
     }, this);
 };
 
-Scene_ItemBase.prototype.applyItem = function() {
-    var action = new Game_Action(this.user());
-    action.setItemObject(this.item());
-    this.itemTargetActors().forEach(function(target) {
-        for (var i = 0; i < action.numRepeats(); i++) {
-            action.apply(target);
+Scene_ItemBase.prototype.applyItem =function(){
+    var action = this.action();
+    var targets = this.itemTargetActors();
+    targets.forEach(function(battler) {
+        var repeats = action.numRepeats();
+        for (var i = 0; i < repeats; i++) {
+            action.apply(battler);                    
         }
-    }, this);
+    });
     action.applyGlobal();
 };
 
