aa0a89c40e4e23fcc623ab70b5d7da18c85ceb6b
krmbn0576 <krmbn0576@gmail.com>
2018-10-12 15:30:58 +0900

    Fix rendering stopped when changed user agent mobile safari to another.

--- js/rpg_managers/SceneManager.js
+++ js/rpg_managers/SceneManager.js
@@ -228,6 +228,7 @@ SceneManager.updateMain = function() {
         this.updateScene();
     } else {
         var newTime = this._getTimeInMsWithoutMobileSafari();
+        if (this._currentTime === undefined) this._currentTime = newTime;
         var fTime = (newTime - this._currentTime) / 1000;
         if (fTime > 0.25) fTime = 0.25;
         this._currentTime = newTime;
